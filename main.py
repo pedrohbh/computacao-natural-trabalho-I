6d93fb91b5e41018c8cc9558fb36fcdc048984b6
Pedro Hoppe <pedrohbh@github.com>
2021-04-03 22:28:11 -0300

    Em desenvolvimento - parte 1

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+iris = pd.read_csv('iris.csv')
+
+#Processo de normatização dos dados
+for c in iris.columns:
+    if c not in ['class']:
+        c_max = max(iris[c])
+        c_min = min(iris[c])
+        iris[c] = (iris[c] - c_min) / (c_max - c_min)
+
+for c in iris.columns:
+    if c in ['class']:
+        dummies = pd.get_dummies(iris[c], prefix=c)
+        iris = iris.join(dummies)
+
+print('head():'); print(iris.head())
+print('-----------------------------------------------')
+
+print('tail():'); print(iris.tail())
+print('-----------------------------------------------')
+
+#Salva arquivo para teste
+iris.to_csv("iris_teste.csv", sep=',', index=False)
+
+#Fim do processo de normatização dos dados
