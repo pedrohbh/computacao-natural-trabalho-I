9e9d845666991633f928647b0896c6dd37e7797e
Pedro Hoppe <pedrohbh@github.com>
2021-04-05 14:45:23 -0300

    Adequação de arquivos

--- main.py
+++ main.py
@@ -23,79 +23,123 @@ print('-----------------------------------------------')
 print('tail():'); print(iris.tail())
 print('-----------------------------------------------')
 
-#Salva arquivo para teste
-#iris.to_csv("iris_teste.csv", sep=',', index=False)
-
-#Fim do processo de normatização dos dados
-
-#Início do código classificador Fuzzy
 def fuzzificar(w):
-    sepal_length = ctrl.Antecedent(np.arange(0.0, 1.1, 0.1), 'sepal_length')
-    sepal_width = ctrl.Antecedent(np.arange(0.0, 1.1, 0.1), 'sepal_width')
-    petal_length = ctrl.Antecedent(np.arange(0.0, 1.1, 0.1), 'petal_length')
-    petal_width = ctrl.Antecedent(np.arange(0.0, 1.1, 0.1), 'petal_width')
+    sepal_length = np.arange(0, 1.01, 0.01)
+    sepal_width = np.arange(0, 1.01, 0.01)
+    petal_length = np.arange(0, 1.01, 0.01)
+    petal_width = np.arange(0, 1.01, 0.01)
 
-    classe_flor = ctrl.Consequent(np.arange(0, 11, 1), 'classe_flor')
+    sepal_length_short = fuzz.trimf(sepal_length, [0, 0, w])
+    sepal_length_middle = fuzz.trimf(sepal_length, [0, w, 1])
+    sepal_length_long = fuzz.trimf(sepal_length, [w, 1, 1])
 
-    classe_flor['setosa'] = fuzz.trimf(classe_flor.universe, [0, 0, 5])
-    classe_flor['versicolor'] = fuzz.trimf(classe_flor.universe, [0, 5, 10])
-    classe_flor['virginica'] = fuzz.trimf(classe_flor.universe, [5, 10, 10])
+    sepal_width_short = fuzz.trimf(sepal_width, [0, 0, w])
+    sepal_width_middle = fuzz.trimf(sepal_width, [0, w, 1])
+    sepal_width_long = fuzz.trimf(sepal_width, [w, 1, 1])
 
-    classe_flor.view()
+    petal_length_short = fuzz.trimf(petal_length, [0, 0, w])
+    petal_length_middle = fuzz.trimf(petal_length, [0, w, 1])
+    petal_length_long = fuzz.trimf(petal_length, [w, 1, 1])
 
-    for elemento in [sepal_length, sepal_width, petal_length, petal_width]:
-        elemento['short'] = fuzz.trimf(elemento.universe, [0.0, 0.0, w])
-        elemento['middle'] = fuzz.trimf(elemento.universe, [0.0, w, 1.0])
-        elemento['long'] = fuzz.trimf(elemento.universe, [w, 1.0, 1.0])
-
-    figura = sepal_length.view()
-    #figura.savefig('exemplo.png')
-
-    x1 = sepal_length
-    x2 = sepal_width
-    x3 = petal_length
-    x4 = petal_width
-
-    rule1 = ctrl.Rule( ( x1['short'] | x1['long'] ) & ( x2['middle'] | x2['long'] ) & ( x3['middle'] | x3['long'] ) & ( x4['middle'] ), classe_flor['versicolor'] )
-    rule2 = ctrl.Rule( ( x3['short'] | x3['middle'] ) & ( x4['short'] ), classe_flor['setosa'] )
-    rule3 = ctrl.Rule( ( x2['short'] | x2['middle'] ) & ( x3['long'] ) & ( x4['long'] ), classe_flor['virginica'] )
-    rule4 = ctrl.Rule( ( x1['middle'] ) & ( x2['short'] | x2['middle'] ) & ( x3['short'] ) & ( x4['long'] ), classe_flor['versicolor'] )
-
-    classe_flores_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
-
-    classe_resposta = ctrl.ControlSystemSimulation(classe_flores_ctrl)
+    petal_width_short = fuzz.trimf(petal_width, [0, 0, w])
+    petal_width_middle = fuzz.trimf(petal_width, [0, w, 1])
+    petal_width_long = fuzz.trimf(petal_width, [w, 1, 1])
 
     N = iris.shape[0]
-    print(N)
+
     acertos = 0
+
     for i in range(0, N):
-        participante = iris.iloc[[i], :]
-        for nome in [ 'sepal_length', 'sepal_width', 'petal_length', 'petal_width']:
-            classe_resposta.input[nome] = participante[nome]
-        #print(classe_resposta.input)
+        participante = iris.iloc[[i],:]
 
-        classe_resposta.compute()
-        #print(classe_resposta.output['classe_flor'])
-        resultado_predito = classe_resposta.output['classe_flor']
-        classe_predita = ""
-        if resultado_predito <= 2.5:
-            classe_predita = 'class_Iris-setosa'
-        elif resultado_predito > 2.5 and resultado_predito <= 7.5:
-            classe_predita = 'class_Iris-versicolor'
-        else:
-            classe_predita = 'class_Iris-virginica'
-        print(classe_predita)
-        Y = iris[classe_predita]
+        #Sepal Length
+        participante_sepal_length = participante['sepal_length']
+        print(participante_sepal_length)
+
+        x1_short = fuzz.interp_membership(sepal_length, sepal_length_short, participante_sepal_length)
+        x1_middle = fuzz.interp_membership(sepal_length, sepal_length_middle, participante_sepal_length)
+        x1_long = fuzz.interp_membership(sepal_length, sepal_length_long, participante_sepal_length)
+
+
+        participante_sepal_width = participante['sepal_width']
+        print(participante_sepal_width)
+
+        x2_short = fuzz.interp_membership(sepal_width, sepal_width_short, participante_sepal_width)
+        x2_middle = fuzz.interp_membership(sepal_width, sepal_width_middle, participante_sepal_width)
+        x2_long = fuzz.interp_membership(sepal_width, sepal_width_long, participante_sepal_width)
+
+        ##Prints para teste
+        #print(x2_short)
+        #print(x2_middle)
+        #print(x2_long)
+
+        participante_petal_length = participante['petal_length']
+        print(participante_petal_length)
+
+        x3_short = fuzz.interp_membership(petal_length, petal_length_short, participante_petal_length)
+        x3_middle = fuzz.interp_membership(petal_length, petal_length_middle, participante_petal_length)
+        x3_long = fuzz.interp_membership(petal_length, petal_length_long, participante_petal_length)
+
+        ##Prints para teste
+        #print(x3_short)
+        #print(x3_middle)
+        #print(x3_long)
+
+        participante_petal_width = participante['petal_width']
+        print(participante_petal_width)
+
+        x4_short = fuzz.interp_membership(petal_width, petal_width_short, participante_petal_width)
+        x4_middle = fuzz.interp_membership(petal_width, petal_width_middle, participante_petal_width)
+        x4_long = fuzz.interp_membership(petal_width, petal_width_long, participante_petal_width)
+
+        rule1 = min(max(x1_short, x1_long), max(x2_middle, x2_long), max(x3_middle, x3_long), x4_middle)
+        rule2 = min(max(x3_short,x3_middle),x4_short)
+        rule3 = min(max(x2_short, x2_middle),x3_long,x4_long)
+        rule4 = min(x1_middle, max(x2_short, x2_middle), x3_short, x4_long)
+        print(rule1)
+        print(rule2)
+        print(rule3)
+        print(rule4)
+
+        vetor_resposta = [rule1, rule2, rule3, rule4]
+        indice_maximo = vetor_resposta.index(max(vetor_resposta))
+        print(indice_maximo)
+        classe_resposta = ""
+        if indice_maximo == 0 or indice_maximo == 3:
+            classe_resposta = "class_Iris-versicolor"
+        elif indice_maximo == 1:
+            classe_resposta = "class_Iris-setosa"
+        elif indice_maximo == 2:
+            classe_resposta = 'class_Iris-virginica'
+        print(classe_resposta)
+
+
+        
+        Y = iris[classe_resposta]
         Y_teste = Y.iloc[i]
-
-        if (Y_teste == 1):
+        if ( Y_teste == 1):
             acertos += 1
-            print("Acertou")
+            print("acertou")
         else:
-            print("Errou")
+            print("errou")
+    
+    acuracia = acertos / N
+    print('acurácia = ',round(acuracia,2))
+    return acuracia
 
-    total_acerto = (acertos / N)
-    print("Total acertado: ", round(total_acerto, 2))
+
+
+
+
+
+
+
+
+
+
 
 
 fuzzificar(0.6)
+
+
+
