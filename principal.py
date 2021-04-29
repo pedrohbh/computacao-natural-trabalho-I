ad467f01ce0e614c365c011c367c192f2ce08f8f
Pedro Hoppe <pedrohbh@github.com>
2021-04-29 16:12:13 -0300

    Adicionar o imprimir do classificador - parte 4

--- principal.py
+++ principal.py
@@ -186,10 +186,11 @@ def fuzzificar(w1, w2, w3, w4):
     return acuracia
 
 fuzzificar(0.6, 0.6, 0.6, 0.6)
-
+"""
 #Descomente para imprimir os classificadores
 #DE
 imprime_classificador_fuzzy(0.41658349, 0.44478748, 0.56807423, 0.21583088)
 
 #PSO
 imprime_classificador_fuzzy(0.99853869, 0.68652673, 0.26936029, 0.41654659)
+"""
